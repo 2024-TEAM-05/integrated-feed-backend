4be44a73cc7f9604a9ad8c8edf0f15635d766aaf
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-22 03:12:43 +0900

    feat: 기본 비즈니스 로직 에러를 핸들링 할 custom Exception 생성 (#3)

---
+++ src/main/java/team05/integrated_feed_backend/exception/custom/BusinessException.java
@@ -0,0 +1,21 @@
+package team05.integrated_feed_backend.exception.custom;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import team05.integrated_feed_backend.exception.code.StatusCode;
+
+/**
+ * 비즈니스 로직 중에서 나는 에러 정의
+ **/
+@Getter
+@AllArgsConstructor
+public class BusinessException extends RuntimeException {
+
+	private final StatusCode statusCode;
+	private String message;
+
+	public BusinessException(StatusCode statusCode) {
+		this.statusCode = statusCode;
+		this.message = statusCode.getMessage();
+	}
+}
