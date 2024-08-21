648073d4dc52c7fff1550198fed52c3ce95bf712
toughCircle <toughCircle@gmail.com>
2024-08-21 12:13:22 +0900

    Initial project structure setup

---
+++ src/main/java/team05/integrated_feed_backend/IntegratedFeedBackendApplication.java
@@ -0,0 +1,13 @@
+package team05.integrated_feed_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegratedFeedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegratedFeedBackendApplication.class, args);
+	}
+
+}
