3632343ac55063c20d1bb2d6e17f32c01cec30f5
Doeun <id5eun30@gmail.com>
2024-08-22 15:52:29 +0900

    feat: BaseEntity 정의 (#5)
    - BaseEntity 정의하여 공통 필드 `createdAt`, `updatedAt` 작성

--- src/main/java/team05/integrated_feed_backend/IntegratedFeedBackendApplication.java
+++ src/main/java/team05/integrated_feed_backend/IntegratedFeedBackendApplication.java
@@ -2,8 +2,9 @@ package team05.integrated_feed_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class IntegratedFeedBackendApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/team05/integrated_feed_backend/core/entity/BaseEntity.java
@@ -0,0 +1,29 @@
+package team05.integrated_feed_backend.core.entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
