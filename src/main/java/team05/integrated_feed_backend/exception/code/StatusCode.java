a79fe01c0e0c42aac104672930a468126579fadd
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-23 13:58:43 +0900

    remove: 사용하지 않는 상태 코드 제거 (#3)

--- src/main/java/team05/integrated_feed_backend/exception/code/StatusCode.java
+++ src/main/java/team05/integrated_feed_backend/exception/code/StatusCode.java
@@ -16,16 +16,12 @@ public enum StatusCode {
 	/**
 	 * 400 번대 CODE
 	 **/
-	ILLEGAL_ARGUMENT(HttpStatus.BAD_REQUEST, "메서드에 잘못된 인자가 전달되었습니다."),
-	RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 리소스를 찾을 수 없습니다."),
 	METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "요청 경로가 지원되지 않습니다."),
 
 	/**
 	 * 500 번대 CODE
 	 **/
-	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류가 발생했습니다."),
-	UNEXPECT_INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예상치 못한 내부 서버 오류가 발생했습니다."),
-	UNSUPPORTED_OPERATION(HttpStatus.NOT_IMPLEMENTED, "지원되지 않는 작업입니다.");
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류가 발생했습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
@@ -35,11 +31,11 @@ public enum StatusCode {
 		this.message = message;
 	}
 
-	public static StatusCode getStatusCode(String name) {
+	public static StatusCode getStatusCode(String name, StatusCode defaultStatusCode) {
 		try {
 			return StatusCode.valueOf(name);
 		} catch (IllegalArgumentException e) {
-			return null;
+			return defaultStatusCode;
 		}
 	}
 }
