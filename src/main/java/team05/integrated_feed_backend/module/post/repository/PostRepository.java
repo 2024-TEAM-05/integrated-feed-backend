878a4ad702a15b6799dcb197394b6fe53436f000
jooda00 <dragon2000a@gmail.com>
2024-08-24 00:49:43 +0900

    feat: PostRepository 생성 (#12)

---
+++ src/main/java/team05/integrated_feed_backend/module/post/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package team05.integrated_feed_backend.module.post.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import team05.integrated_feed_backend.module.post.entity.Post;
+
+public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository {
+}
