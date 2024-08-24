1fc62d1db2570915cfbe2a1128c9e23d2bd98347
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-25 08:48:52 +0900

    feat: PostRepository 추가 (#22)

---
+++ src/main/java/team05/integrated_feed_backend/module/post/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package team05.integrated_feed_backend.module.post.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import team05.integrated_feed_backend.module.post.entity.Post;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
