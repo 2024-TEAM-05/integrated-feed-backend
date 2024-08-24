49e73333aa3ac0457dcaa4fd4a26d08afcf5231c
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-25 08:50:54 +0900

    feat: 좋아요, 공유 수 올리는 api 추가 (#22)

--- src/main/java/team05/integrated_feed_backend/module/post/controller/PostController.java
+++ src/main/java/team05/integrated_feed_backend/module/post/controller/PostController.java
@@ -3,7 +3,10 @@ package team05.integrated_feed_backend.module.post.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -11,11 +14,14 @@ import team05.integrated_feed_backend.common.BaseApiResponse;
 import team05.integrated_feed_backend.exception.code.StatusCode;
 import team05.integrated_feed_backend.module.post.dto.request.PostSearchReq;
 import team05.integrated_feed_backend.module.post.dto.response.PostSearchRes;
+import team05.integrated_feed_backend.module.post.service.PostService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/posts")
 public class PostController implements PostControllerDocs {
+
+	private final PostService postService;
 
 	@Override
 	@GetMapping
@@ -26,6 +32,22 @@ public class PostController implements PostControllerDocs {
 		PostSearchRes res = new PostSearchRes();
 
 		return new BaseApiResponse<>(HttpStatus.OK, StatusCode.OK.getMessage(), res);
+	}
+
+	// 좋아요 수 증가시키는 api
+	@ResponseStatus(HttpStatus.OK)
+	@PatchMapping("/{postId}/like")
+	public BaseApiResponse<Void> increaseLikeCount(@PathVariable(name = "postId") Long postId) {
+		postService.increaseLikeCount(postId);
+		return BaseApiResponse.of(HttpStatus.OK);
+	}
+
+	// 공유 수 증가시키는 api
+	@ResponseStatus(HttpStatus.OK)
+	@PatchMapping("/{postId}/share")
+	public BaseApiResponse<Void> increaseShareCount(@PathVariable(name = "postId") Long postId) {
+		postService.increaseShareCount(postId);
+		return BaseApiResponse.of(HttpStatus.OK);
 	}
 
 }
