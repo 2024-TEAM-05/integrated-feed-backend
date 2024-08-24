1518188299427c8339ca82c8e32af2d348beb581
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-25 08:48:20 +0900

    feat: 이벤트 수신하여 외부 api 호출하기 위한 이벤트 리스너 추가 (#22)

---
+++ src/main/java/team05/integrated_feed_backend/module/post/event/listener/PostEventListener.java
@@ -0,0 +1,47 @@
+package team05.integrated_feed_backend.module.post.event.listener;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import team05.integrated_feed_backend.infra.sns.adapter.FacebookAdapter;
+import team05.integrated_feed_backend.infra.sns.adapter.InstagramAdapter;
+import team05.integrated_feed_backend.infra.sns.adapter.TwitterAdapter;
+import team05.integrated_feed_backend.module.post.event.LikeCountIncreaseEvent;
+import team05.integrated_feed_backend.module.post.event.ShareCountIncreaseEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class PostEventListener {
+
+	private final FacebookAdapter facebookAdapter;
+	private final TwitterAdapter twitterAdapter;
+	private final InstagramAdapter instagramAdapter;
+
+	@Async
+	@EventListener
+	public void handleLikeCountIncreaseEvent(LikeCountIncreaseEvent event) {
+		Long postId = event.getPostId();
+		log.info("Asynchronously handling like count increase event for post ID: {}", postId);
+
+		// 외부 API 호출
+		facebookAdapter.increaseLikeCount(postId);
+		twitterAdapter.increaseLikeCount(postId);
+		instagramAdapter.increaseLikeCount(postId);
+	}
+
+	@Async
+	@EventListener
+	public void handleShareCountIncreaseEvent(ShareCountIncreaseEvent event) {
+		Long postId = event.getPostId();
+		log.info("Asynchronously handling share count increase event for post ID: {}", postId);
+
+		// 외부 API 호출
+		facebookAdapter.increaseShareCount(postId);
+		twitterAdapter.increaseShareCount(postId);
+		instagramAdapter.increaseShareCount(postId);
+	}
+}
