d336d85713dee8efd6af6c58dc9301ef03dd7276
jooda00 <dragon2000a@gmail.com>
2024-08-24 09:26:35 +0900

    refactor: getPostStatistics 메소드 응답 시 StatusCode 활용 (#12)

--- src/main/java/team05/integrated_feed_backend/module/post/controller/PostController.java
+++ src/main/java/team05/integrated_feed_backend/module/post/controller/PostController.java
@@ -2,7 +2,6 @@ package team05.integrated_feed_backend.module.post.controller;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import team05.integrated_feed_backend.common.BaseApiResponse;
+import team05.integrated_feed_backend.exception.code.StatusCode;
 import team05.integrated_feed_backend.module.post.dto.request.PostStatisticsListReq;
 import team05.integrated_feed_backend.module.post.dto.response.PostStatisticsListRes;
 import team05.integrated_feed_backend.module.post.service.PostService;
@@ -29,6 +29,6 @@ public class PostController {
 		@RequestParam(required = false) String end) {
 		PostStatisticsListReq request = new PostStatisticsListReq(type, hashtag, value, start, end);
 		List<PostStatisticsListRes> res = postService.getPostStatistics(request);
-		return new BaseApiResponse<>(HttpStatus.OK, "요청이 성공했습니다.", res);
+		return new BaseApiResponse<>(StatusCode.OK.getHttpStatus(), StatusCode.OK.getMessage(), res);
 	}
 }
