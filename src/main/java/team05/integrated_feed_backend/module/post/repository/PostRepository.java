5024a34f3d99a8328bc9a2c2788a6370b634213e
jooda00 <dragon2000a@gmail.com>
2024-08-24 12:22:03 +0900

    remove: PostRepository, PostCustomRepository 클래스 삭제 (#12)
    - pr 날릴 때 빌드 에러

--- src/main/java/team05/integrated_feed_backend/module/post/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package team05.integrated_feed_backend.module.post.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import team05.integrated_feed_backend.module.post.entity.Post;
-
-public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository {
-}
