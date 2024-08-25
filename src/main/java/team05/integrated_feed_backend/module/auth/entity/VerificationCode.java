551177525ed4a1f5fa5f9b8f0b64ff27680583b8
uijin-j <uijin0531@gmail.com>
2024-08-25 20:58:31 +0900

    feat: VerificationCode 정적 팩토리 메서드 추가 및 Builder 접근 제어자 Private으로 변경

--- src/main/java/team05/integrated_feed_backend/module/auth/entity/VerificationCode.java
+++ src/main/java/team05/integrated_feed_backend/module/auth/entity/VerificationCode.java
@@ -1,29 +1,53 @@
 package team05.integrated_feed_backend.module.auth.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import team05.integrated_feed_backend.common.BaseEntity;
 import team05.integrated_feed_backend.module.user.entity.Member;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder
+@Builder(access = AccessLevel.PRIVATE)
 public class VerificationCode extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long verificationCodeId;
+	private static final int EXPIRE_IN_DAYS = 1;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long verificationCodeId;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id", nullable = false)
-    private Member member;
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
 
-    @Column(nullable = false)
-    private String code;
+	@Column(nullable = false)
+	private String code;
 
-    @Column(nullable = false)
-    private LocalDateTime expiredAt;
+	@Column(nullable = false)
+	private LocalDateTime expiredAt;
+
+	public static VerificationCode of(Member member, String code, LocalDateTime issuedAt) {
+		return VerificationCode.builder()
+			.member(member)
+			.code(code)
+			.expiredAt(calculateExpiredAt(issuedAt))
+			.build();
+	}
+
+	private static LocalDateTime calculateExpiredAt(LocalDateTime issuedAt) {
+		return issuedAt.plusDays(EXPIRE_IN_DAYS);
+	}
 }
