29f091f7c312312bbae36a2cb66d76fd3731d8ac
jooda00 <dragon2000a@gmail.com>
2024-08-26 00:39:33 +0900

    feat: PostStatisticsService 메소드 작성 (#31)
    - getPostStatistics 메소드

--- src/main/java/team05/integrated_feed_backend/module/post/service/PostStatisticsService.java
+++ src/main/java/team05/integrated_feed_backend/module/post/service/PostStatisticsService.java
@@ -8,16 +8,19 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import team05.integrated_feed_backend.module.post.dto.request.PostStatisticsListReq;
 import team05.integrated_feed_backend.module.post.dto.response.PostStatisticsListRes;
+import team05.integrated_feed_backend.module.post.repository.PostRepository;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PostStatisticsService {
+
+	private final PostRepository postRepository;
+
 	public List<PostStatisticsListRes> getPostStatistics(PostStatisticsListReq request) {
 		// 예외 처리
 		// 요청에 따른 통계 데이터 조회
-		// 일단 repository에서 메소드가 완성이 안되어서 List.of()로 넣었습니다.
-		List<PostStatisticsListRes> statistics = List.of();
+		List<PostStatisticsListRes> statistics = postRepository.findPostStatisticsByQueryParameter(request);
 		return statistics;
 	}
 }
