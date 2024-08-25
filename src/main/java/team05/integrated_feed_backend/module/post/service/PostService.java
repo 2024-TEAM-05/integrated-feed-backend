fc734ac4cc1c4d4f52064852ca794e2431848966
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-25 10:00:28 +0900

    refactor: 사용하지 않는 어노테이션 제거 (#22)

--- src/main/java/team05/integrated_feed_backend/module/post/service/PostService.java
+++ src/main/java/team05/integrated_feed_backend/module/post/service/PostService.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import team05.integrated_feed_backend.exception.code.StatusCode;
 import team05.integrated_feed_backend.exception.custom.DataNotFoundException;
 import team05.integrated_feed_backend.module.post.entity.Post;
@@ -14,7 +13,6 @@ import team05.integrated_feed_backend.module.post.repository.PostRepository;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-@Slf4j
 public class PostService {
 
 	private final PostRepository postRepository;
