588fab1d3d2609315feb7dba8b120fdbb1633356
hye-on <ain0103@naver.com>
2024-08-26 02:12:38 +0900

    chore: Qclass의 이름 중복으로 인해 hashtag테이블의 hashtag칼럼 hashtagName으로 변경

--- src/main/java/team05/integrated_feed_backend/module/post/entity/Hashtag.java
+++ src/main/java/team05/integrated_feed_backend/module/post/entity/Hashtag.java
@@ -1,22 +1,13 @@
 package team05.integrated_feed_backend.module.post.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 import team05.integrated_feed_backend.common.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -28,8 +19,8 @@ public class Hashtag extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long hashtagId;
 
-	@Column(nullable = false, unique = true)
-	private String hashtag;
+	@Column(name = "hashtag_name", nullable = false, unique = true)
+	private String hashtagName;
 
 	@OneToMany(mappedBy = "hashtag", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
 	@Builder.Default
