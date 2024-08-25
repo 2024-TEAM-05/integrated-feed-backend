a0b717cd50d0012fb96f7e8afc3f145a6f831e3e
hye-on <ain0103@naver.com>
2024-08-26 02:16:32 +0900

    feat: PostController에 서비스 호출 추가

--- src/main/java/team05/integrated_feed_backend/module/post/controller/PostController.java
+++ src/main/java/team05/integrated_feed_backend/module/post/controller/PostController.java
@@ -1,6 +1,5 @@
 package team05.integrated_feed_backend.module.post.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import team05.integrated_feed_backend.common.BaseApiResponse;
 import team05.integrated_feed_backend.exception.code.StatusCode;
+import team05.integrated_feed_backend.module.post.PostService;
 import team05.integrated_feed_backend.module.post.dto.request.PostSearchReq;
 import team05.integrated_feed_backend.module.post.dto.response.PostSearchRes;
 
@@ -17,15 +17,15 @@ import team05.integrated_feed_backend.module.post.dto.response.PostSearchRes;
 @RequestMapping("/api/posts")
 public class PostController implements PostControllerDocs {
 
+	private final PostService postService;
+
 	@Override
 	@GetMapping
 	public BaseApiResponse<PostSearchRes> getPosts(
 		@ModelAttribute PostSearchReq postSearchReq
 	) {
 
-		PostSearchRes res = new PostSearchRes();
-
-		return new BaseApiResponse<>(HttpStatus.OK, StatusCode.OK.getMessage(), res);
+		return BaseApiResponse.of(StatusCode.OK, postService.getPosts(postSearchReq));
 	}
 
 }
