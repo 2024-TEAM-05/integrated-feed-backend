35f497422ea752891dbb3f2afce2685f3a5d85e8
uijin-j <uijin0531@gmail.com>
2024-08-25 20:41:49 +0900

    feat: Member 빌더 액세스 레벨 Private으로 변경 및 정적 팩터리 메서드 추가

--- src/main/java/team05/integrated_feed_backend/module/user/entity/Member.java
+++ src/main/java/team05/integrated_feed_backend/module/user/entity/Member.java
@@ -2,25 +2,40 @@ package team05.integrated_feed_backend.module.user.entity;
 
 import static team05.integrated_feed_backend.module.user.entity.enums.MemberStatus.*;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import team05.integrated_feed_backend.common.BaseEntity;
 import team05.integrated_feed_backend.module.user.entity.enums.MemberStatus;
+import team05.integrated_feed_backend.module.user.entity.vo.Email;
 import team05.integrated_feed_backend.module.user.entity.vo.Password;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder
+@Builder(access = AccessLevel.PRIVATE)
 public class Member extends BaseEntity {
 	public static final MemberStatus DEFAULT_MEMBER_STATUS = UNVERIFIED;
 
-    private Long memberId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long memberId;
 
-    @Column(nullable = false, unique = true)
-    private String account;
+	@Column(nullable = false, unique = true)
+	private String account;
 
 	@Embedded
 	@Column(nullable = false)
@@ -33,4 +48,13 @@ public class Member extends BaseEntity {
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private MemberStatus status;
+
+	public static Member signUp(String account, String password, String email, PasswordEncoder passwordEncoder) {
+		return Member.builder()
+			.account(account)
+			.password(Password.of(password, passwordEncoder))
+			.email(Email.from(email))
+			.status(DEFAULT_MEMBER_STATUS)
+			.build();
+	}
 }
