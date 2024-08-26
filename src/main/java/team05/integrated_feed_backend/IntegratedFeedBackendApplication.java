315bbec45af80c22c6440253331027502789b77c
hye-on <ain0103@naver.com>
2024-08-26 12:58:11 +0900

    feat: BaseEntity를 위한 @EnableJpaAuditing 어노테이션 추가

--- src/main/java/team05/integrated_feed_backend/IntegratedFeedBackendApplication.java
+++ src/main/java/team05/integrated_feed_backend/IntegratedFeedBackendApplication.java
@@ -3,8 +3,10 @@ package team05.integrated_feed_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @EnableFeignClients(basePackages = "team05.integrated_feed_backend.infra.sns.api")
 public class IntegratedFeedBackendApplication {
 
