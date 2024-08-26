ff94cb2b6c5a4ffef81efb9d27adfb7d28217406
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-26 12:56:12 +0900

    fix: 게시물 SocialType 에 맞게 외부 api 호출 (#22)

--- src/main/java/team05/integrated_feed_backend/module/post/event/listener/PostEventListener.java
+++ src/main/java/team05/integrated_feed_backend/module/post/event/listener/PostEventListener.java
@@ -1,15 +1,17 @@
 package team05.integrated_feed_backend.module.post.event.listener;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import team05.integrated_feed_backend.common.enums.SocialMediaType;
 import team05.integrated_feed_backend.infra.sns.adapter.FacebookAdapter;
 import team05.integrated_feed_backend.infra.sns.adapter.InstagramAdapter;
 import team05.integrated_feed_backend.infra.sns.adapter.TwitterAdapter;
-import team05.integrated_feed_backend.module.post.event.LikeCountIncreaseEvent;
-import team05.integrated_feed_backend.module.post.event.ShareCountIncreaseEvent;
+import team05.integrated_feed_backend.module.post.event.LikeCountIncreasedEvent;
+import team05.integrated_feed_backend.module.post.event.ShareCountIncreasedEvent;
 
 @Component
 @RequiredArgsConstructor
@@ -20,25 +22,35 @@ public class PostEventListener {
 	private final TwitterAdapter twitterAdapter;
 	private final InstagramAdapter instagramAdapter;
 
+	@Async
 	@EventListener
-	public void handleLikeCountIncreaseEvent(LikeCountIncreaseEvent event) {
+	public void handleLikeCountIncreasedEvent(LikeCountIncreasedEvent event) {
 		Long postId = event.getPostId();
+		SocialMediaType type = event.getType();
 		log.info("Asynchronously handling like count increase event for post ID: {}", postId);
 
 		// 외부 API 호출
-		facebookAdapter.increaseLikeCount(postId);
-		twitterAdapter.increaseLikeCount(postId);
-		instagramAdapter.increaseLikeCount(postId);
+		switch (type) {
+			case FACEBOOK -> facebookAdapter.increaseLikeCount(postId);
+			case INSTAGRAM -> instagramAdapter.increaseLikeCount(postId);
+			case TWITTER -> twitterAdapter.increaseLikeCount(postId);
+			default -> log.error(postId + " 게시물의 " + type + ": facebook, instagram, twitter 중 하나로 설정되어 있지 않습니다.");
+		}
 	}
 
+	@Async
 	@EventListener
-	public void handleShareCountIncreaseEvent(ShareCountIncreaseEvent event) {
+	public void handleShareCountIncreasedEvent(ShareCountIncreasedEvent event) {
 		Long postId = event.getPostId();
+		SocialMediaType type = event.getType();
 		log.info("Asynchronously handling share count increase event for post ID: {}", postId);
 
 		// 외부 API 호출
-		facebookAdapter.increaseShareCount(postId);
-		twitterAdapter.increaseShareCount(postId);
-		instagramAdapter.increaseShareCount(postId);
+		switch (type) {
+			case FACEBOOK -> facebookAdapter.increaseShareCount(postId);
+			case INSTAGRAM -> instagramAdapter.increaseShareCount(postId);
+			case TWITTER -> twitterAdapter.increaseShareCount(postId);
+			default -> log.error(postId + " 게시물의 " + type + ": facebook, instagram, twitter 중 하나로 설정되어 있지 않습니다.");
+		}
 	}
 }
