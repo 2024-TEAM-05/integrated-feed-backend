89a32156aad8c3c03f31907b302c4d129d31c9e7
toughCircle <toughCircle@gmail.com>
2024-08-26 09:51:54 +0900

    refactor: 조회 반환값 형식을 Optional로 변경하여 예외처리 개선 (#20)

--- src/main/java/team05/integrated_feed_backend/module/post/repository/PostRepositoryCustom.java
+++ src/main/java/team05/integrated_feed_backend/module/post/repository/PostRepositoryCustom.java
@@ -1,9 +1,11 @@
 package team05.integrated_feed_backend.module.post.repository;
 
+import java.util.Optional;
+
 import team05.integrated_feed_backend.module.post.entity.Post;
 
 public interface PostRepositoryCustom {
 
-	Post findDetailPostById(Long id);
-	
+	Optional<Post> findDetailPostById(Long id);
+
 }
