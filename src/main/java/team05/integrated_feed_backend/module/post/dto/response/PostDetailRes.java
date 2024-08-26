5f57ba52c3fc6c2d6ef8d8228f344ad725b68141
toughCircle <toughCircle@gmail.com>
2024-08-26 17:50:10 +0900

    chore: 응답 필드명 Camel case 사용 (#20)

--- src/main/java/team05/integrated_feed_backend/module/post/dto/response/PostDetailRes.java
+++ src/main/java/team05/integrated_feed_backend/module/post/dto/response/PostDetailRes.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
@@ -17,36 +16,28 @@ import lombok.NoArgsConstructor;
 public class PostDetailRes {
 
 	@Schema(description = "게시물 id")
-	@JsonProperty("post_id")
 	private Long postId;
 
 	@Schema(description = "게시물 제목")
-	@JsonProperty("post_title")
 	private String title;
 
 	@Schema(description = "게시물 내용")
-	@JsonProperty("post_content")
 	private String content;
 
 	@Schema(description = "조회 수")
-	@JsonProperty("view_count")
 	private Long viewCount;
 
 	@Schema(description = "좋아요 수")
-	@JsonProperty("like_count")
 	private Long likeCount;
 
 	@Schema(description = "공유 수")
-	@JsonProperty("share_count")
 	private Long shareCount;
 
 	@Schema(description = "생성일")
-	@JsonProperty("created_at")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
 	private LocalDateTime createdAt;
 
 	@Schema(description = "수정일")
-	@JsonProperty("updated_at")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
 	private LocalDateTime updatedAt;
 
