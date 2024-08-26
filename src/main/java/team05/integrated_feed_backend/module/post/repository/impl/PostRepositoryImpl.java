0bcb070c506f6980a3b17945c31d5680e4f2768d
toughCircle <toughCircle@gmail.com>
2024-08-26 09:58:02 +0900

    refactor: Query 수정 (#20)

--- src/main/java/team05/integrated_feed_backend/module/post/repository/impl/PostRepositoryImpl.java
+++ src/main/java/team05/integrated_feed_backend/module/post/repository/impl/PostRepositoryImpl.java
@@ -1,12 +1,16 @@
 package team05.integrated_feed_backend.module.post.repository.impl;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
 import team05.integrated_feed_backend.module.post.entity.Post;
+import team05.integrated_feed_backend.module.post.entity.QHashtag;
 import team05.integrated_feed_backend.module.post.entity.QPost;
+import team05.integrated_feed_backend.module.post.entity.QPostHashtag;
 import team05.integrated_feed_backend.module.post.repository.PostRepositoryCustom;
 
 @Repository
@@ -16,13 +20,20 @@ public class PostRepositoryImpl implements PostRepositoryCustom {
 	private final JPAQueryFactory jpaQueryFactory;
 
 	@Override
-	public Post findDetailPostById(Long id) {
+	public Optional<Post> findDetailPostById(Long id) {
 
 		QPost post = QPost.post;
-		return jpaQueryFactory
-			.selectFrom(post)
-			.where(post.postId.eq(id))
-			.fetchOne();
+		QPostHashtag postHashtag = QPostHashtag.postHashtag;
+		QHashtag hashtag = QHashtag.hashtag1;
 
+		return Optional.ofNullable(
+			jpaQueryFactory
+				.selectFrom(post)
+				.leftJoin(post.postHashtags, postHashtag).fetchJoin()
+				.leftJoin(postHashtag.hashtag, hashtag).fetchJoin()
+				.where(post.postId.eq(id))
+				.fetchOne()
+		);
+		
 	}
 }
