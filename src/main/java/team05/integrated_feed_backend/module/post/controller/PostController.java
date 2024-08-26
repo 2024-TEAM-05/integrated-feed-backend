a0de7805e5fa3c1df23f53f875eab1e5389f589b
Doeun <id5eun30@gmail.com>
2024-08-26 15:07:26 +0900

    chore: PostController import 수정 (#23)

--- src/main/java/team05/integrated_feed_backend/module/post/controller/PostController.java
+++ src/main/java/team05/integrated_feed_backend/module/post/controller/PostController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import team05.integrated_feed_backend.common.BaseApiResponse;
-import team05.integrated_feed_backend.exception.code.StatusCode;
+import team05.integrated_feed_backend.common.code.StatusCode;
 import team05.integrated_feed_backend.module.post.dto.request.PostSearchReq;
 import team05.integrated_feed_backend.module.post.dto.response.PostSearchRes;
 
