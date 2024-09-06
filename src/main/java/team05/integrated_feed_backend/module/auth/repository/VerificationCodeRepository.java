49cd4b0f83811f0bc60c6c08c94f2b2a55411036
uijin-j <uijin0531@gmail.com>
2024-09-06 16:28:40 +0900

    test: MemberEventListener 테스트 코드 작성

--- src/main/java/team05/integrated_feed_backend/module/auth/repository/VerificationCodeRepository.java
+++ src/main/java/team05/integrated_feed_backend/module/auth/repository/VerificationCodeRepository.java
@@ -1,11 +1,15 @@
 package team05.integrated_feed_backend.module.auth.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import team05.integrated_feed_backend.module.auth.entity.VerificationCode;
+import team05.integrated_feed_backend.module.member.entity.Member;
 
 @Repository
 public interface VerificationCodeRepository extends JpaRepository<VerificationCode, Long> {
-	
+
+	Optional<VerificationCode> findByMember(Member member);
 }
